0fe427af506a129e319fef9037205fe23500df71
ABHISHEK V <112977586+abhi-mwonu@users.noreply.github.com>
2025-02-22 16:12:55 +0530

    Update data.js

--- user-data/data.js
+++ user-data/data.js
@@ -1,8 +1,7 @@
 export const bio = [
-  "Hello, I'm ABHISHEK",
-  "<strong>Passionate Engineering Student</strong>",
-  "I'm currently pursuing a degree in Information Technology, with a strong passion for cybersecurity and reverse engineering.",
-  "Passionate about teamwork, and continuous improvement in engineering processes.",
+  "I'm <strong>Abhishek V</strong>, A Tech and <strong>cybersecurity</strong> enthusiast pursuing B.Tech in IT at GEC Idukki",
+  "Passionate about networking, and security.Always eager to learn, collaborate, and innovate in the tech space",
+  "<strong>Let’s connect !</strong>",
 ];
 
 export const skills = [
@@ -12,61 +11,30 @@ export const skills = [
 
 export const experience = [
   {
-    title: "Shiprocket Private Limited",
-    duration: "September 2023 - Present",
-    subtitle: "Software Engineer",
+    title: "Built a dice roller program to simulate random dice rolls",
+    duration: "",
+    subtitle: "Python",
     details: [
-      "Developed a design system using Stencil and Storybook that provides consistent UI components across platforms, ensuring a cohesive design.",
-      "Developed a dynamic support ticket panel that renders subcategories based on component IDs, handling up to 100 AWBs simultaneously.",
-      "Migrated 30+ modules from AngularJS to Angular 15 using Generative AI tools, reporting directly to the VP of Engineering.",
-      "Designed and implemented advanced features such as keyboard shortcuts, improving operational efficiency by 15 percent.",
-      "Took ownership of OKRs, collaborated with the product team, and mentored two junior developers, cutting their code review iterations and ensuring on-time feature delivery.",
+       "Supported multiple dice rolls with instant results",
+       "Utilized Python's random module (or JavaScript Math.random() for web-based version)", 
+       "Demonstrated skills in random number generation, loops, and user interaction",
     ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "AngularJs"],
+    tags: ["JavaScript"],
     icon: "truck ",
   },
   {
-    title: "Biofourmis India Private Limited",
-    duration: "April 2022 - Jul 2023",
-    subtitle: "Software Engineer",
+    title: "Rondom password generator",
+    duration: "",
+    subtitle: "Python",
     details: [
-      "Spearheaded the implementation of micro-frontends using Module Federation, enabling modular and scalable architecture.",
-      "Earned the Biofourmis Bravo Award for upgrading the application to support flexible operations across multiple geographies.",
-      "Enhanced performance and user experience by integrating Twilio-Video, Countly, and NGXS WebSocket for real-time data management.",
+       "Created a program to generate secure, customizable passwords",
+       "Utilized randomization and string manipulation for varied character combinations", 
+       "Demonstrated skills in Python/JavaScript logic and user input handling",
     ],
     tags: [
-      "JavaScript",
-      "Angular",
-      "RxJS",
-      "NGXS",
-      "TypeScript",
-      "RxState",
-      "Webpack",
-      "Optimization",
+      "Python",
     ],
     icon: "heartbeat",
-  },
-  {
-    title: "Novopay Solutions Private Limited",
-    duration: "June 2020 - April 2022",
-    subtitle: "Software Engineer",
-    details: [
-      "Built critical banking features including AePS services (eKYC, cash withdrawal, balance inquiry) used by over 1,00,000 retailers.",
-      "Developed QR code integration for wallet money loading, streamlining retailer workflows and reducing errors by 25 percent.",
-      "Integrated Clevertap analytics, Freshdesk ticketing, and chatbot services, enhancing user support and engagement.",
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Nodejs", "Jenkins"],
-    icon: "qrcode",
-  },
-  {
-    title: "ThinkPedia LLP",
-    duration: "May 2019 - June 2019",
-    subtitle: "SDE Intern",
-    details: [
-      `Designed and developed a social media post management panel, improving collaboration efficiency by 50 percent.`,
-    ],
-    tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
-    icon: "group",
   },
 ];
 
